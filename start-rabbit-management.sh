1a8e4b52298483b430ae1b11acf746613aad9656
Ronald Dilley <ronald.dilley.ctr@usgs.gov>
2017-04-05 09:15:37 -0500

    updated version

--- start-rabbit-management.sh
+++ start-rabbit-management.sh
@@ -1,1 +1,1 @@
-docker run -d --hostname my-rabbit --name second-rabbit -p 8080:15672 -p 5672:5672 rabbitmq:3-management
+docker run --detach --hostname espa-rabbit --name espa-rabbit -p 8080:15672 -p 5672:5672 rabbitmq:3.6.9-management-alpine
